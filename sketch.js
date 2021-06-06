829b3687e18cbf07a1fd6b238f03b04f46495035
AKSHITH-96 <75170991+AKSHITH-96@users.noreply.github.com>
2021-06-06 20:25:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var hammer;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	hammer = new Hammer(mouseX,mouseY,50,50);
+	ground = new Ground(400,690,800,10);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  
+  hammer.display();
+  ground.display();
+}
+
+
+
